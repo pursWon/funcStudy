cd0113d8494907c8346254fef0308e75cfedeea7
Wongi <hpurs95@gmail.com>
2022-07-17 13:19:25 +0900

    Initial Commit

---
+++ funcStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  funcStudy
+//
+//  Created by MacBook Air on 2022/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ funcStudy/funcStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  funcStudyApp.swift
+//  funcStudy
+//
+//  Created by MacBook Air on 2022/07/17.
+//
+
+import SwiftUI
+
+@main
+struct funcStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
